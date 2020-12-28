bc3469382e9ba60bf2081f90779d7ef65ba8da0d
Miguel Neto <miguelcastroneto@protonmail.com>
2020-12-28 20:28:29 +0000

    Python_1.2.py

--- Python Projects/Python_1.2.py
+++ Python Projects/Python_1.2.py
@@ -4,5 +4,9 @@ rate = input('Enter your rate:')
 flhours = float(hours)
 flrate = float(rate)
 
+if flhours > 40 :
+    print('Overtime')
+else:
+    print('Regular')
 pay = flhours * flrate
 print('Pay:', pay)
