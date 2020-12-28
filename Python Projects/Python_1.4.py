1c008714afb6be6175deaa870bd64a6e96bf5b6a
mnet0 <miguelcastroneto@protonmail.com>
2020-12-28 21:58:31 +0000

    Python_1.4.py

---
+++ Python Projects/Python_1.4.py
@@ -0,0 +1,20 @@
+hours = input('Enter your hours:')
+rate = input('Enter your rate:')
+
+try:
+    flhours = float(hours)
+    flrate = float(rate)
+except:
+    print("Error, please enter numeric input!")
+    quit()
+
+if flhours > 40 :
+    print('Overtime')
+    regularpay = flrate * flhours
+    overtimepay = (flhours - 40.0) * (flrate * 0.5)
+    pay = regularpay + overtimepay
+else:
+    print('Regular')
+    pay = flhours * flrate
+
+print('Pay:', pay)
