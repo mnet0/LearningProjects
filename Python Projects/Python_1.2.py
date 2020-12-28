8a935f6632ea16d6dfc985211685d3bb2b899ccf
mnet0 <miguelcastroneto@protonmail.com>
2020-12-28 20:37:32 +0000

    Python_1.2.py

--- Python Projects/Python_1.2.py
+++ Python Projects/Python_1.2.py
@@ -6,7 +6,11 @@ flrate = float(rate)
 
 if flhours > 40 :
     print('Overtime')
+    regularpay = flrate * flhours
+    overtimepay = (flhours - 40.0) * (flrate * 0.5)
+    pay = regularpay + overtimepay
 else:
     print('Regular')
-pay = flhours * flrate
+    pay = flhours * flrate
+
 print('Pay:', pay)
