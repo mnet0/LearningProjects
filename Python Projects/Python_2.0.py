abb297139b359c7c7fb1565c3a9d851405be3825
mnet0 <miguelcastroneto@protonmail.com>
2020-12-29 18:16:00 +0000

    Commented a few lines for future coding

--- Python Projects/Python_2.0.py
+++ Python Projects/Python_2.0.py
@@ -1,7 +1,9 @@
+# Creating variables
 num = None
 Totalnum = 0
 i = 0
 
+# Main loop
 while num != "done":
     num = input("Enter a number:")
     try:
@@ -10,7 +12,8 @@ while num != "done":
         i = i + 1
     except:
         print("bad data")
-
+# Averaging
 flaverage = Totalnum / i
 
+# Printing Total, Number of inputs and Average!
 print(Totalnum, i, flaverage)
