a87ac7f1116057089b7180db1618c72dc5c46652
mnet0 <miguelcastroneto@protonmail.com>
2020-12-30 13:58:12 +0000

    Finished project 5!

---
+++ Python_5.0.py
@@ -0,0 +1,29 @@
+fname = input('Enter file name: ')
+if len(fname) < 1 : fname = 'intro.txt'
+handle = open(fname)
+
+di = dict()
+
+for line in handle:
+    line = line.replace(',','')
+    line = line.replace('.','')
+    line = line.replace(':','')
+    line = line.replace('!','')
+    line = line.replace('?','')
+    line = line.replace('(','')
+    line = line.replace(')','')
+    line = line.rstrip()
+    line = line.lower()
+    words = line.split()
+    for word in words:       
+        di[word] = di.get(word,0) + 1
+
+largest = -1
+theword = None
+for w,v in di.items() :
+    print(w, v)
+    if v > largest :
+        largest = v
+        theword = w
+
+print('Done',theword, largest)
